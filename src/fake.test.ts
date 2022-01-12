b6342921df82ce59345303f972508e4e703e8dc6
sashaz <6964653+OZahnitko@users.noreply.github.com>
2022-01-12 11:14:38 +1000

    pre cov

---
+++ src/fake.test.ts
@@ -0,0 +1,15 @@
+import { paragraph } from "./fake";
+
+describe("Paragraph tests", () => {
+  test("Function returns a string", () => {
+    expect(typeof paragraph()).toBe("string");
+  });
+
+  test("Default parameters work", () => {
+    expect(paragraph).toBeTruthy();
+  });
+
+  test("Accepts parameters", () => {
+    expect(paragraph({ quantity: 2 })).toBeTruthy();
+  });
+});
